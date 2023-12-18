af0422fb5eb503213fb6cda2c12d11ff478e9bd1
brianmina <brian.mina17@gmail.com>
2023-12-18 09:46:20 -0500

    plotting with scatter function and defining colors were accomplished

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.gist_rainbow, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=14)
@@ -12,4 +14,7 @@ ax.set_ylabel("Square of value", fontsize=14)
 # Set size of tick lable
 ax.tick_params(labelsize=14)
 
+# Set the range for each axis.
+ax.axis([0, 1001, 0, 1_100_000])
+ax.ticklabel_format(style='plain')
 plt.show()
