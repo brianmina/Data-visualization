bc66d15894dcd50ec7cff61045dcfa6a58485dae
brianmina <brian.mina17@gmail.com>
2023-12-19 15:59:04 -0500

    adding color to the star and end point was accomplished

--- random_walk.py
+++ random_walk.py
@@ -1,5 +1,6 @@
 from random import choice
 
+
 class RandomWalk:
     """A class to generate random walks."""
     def __init__(self, num_points=5000):
@@ -29,8 +30,8 @@ class RandomWalk:
                 continue
 
             # Calculate the new position.
-            x = self.x_values[-1].append(x) + x_step
-            y = self.y_values[-1].append(y) + y_step
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
 
             self.x_values.append(x)
             self.y_values.append(y)
