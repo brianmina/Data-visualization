1f5ef99f77d6c3188aad49e09016b23b29a26d8f
brianmina <brian.mina17@gmail.com>
2023-12-23 06:12:23 -0500

    extract data from a CSV file and visualize it Matploti

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,9 +1,29 @@
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
 
 path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+
+# Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+
+# Plot the high temperatures.
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Format plot.
+ax.set_title("Daily High Temperature, July 2021", fontsize=24 )
+ax.set_xlabel(" ", fontsize=16)
+ax.set_ylabel("Temperature(F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
