49468c92ca4a4c76223c82174a32b35b47d2b129
brianmina <brian.mina17@gmail.com>
2023-12-24 06:22:53 -0500

    plotting data using datetime module

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('weather_data/sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
@@ -8,21 +9,23 @@ lines = path.read_text(encoding='utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Extract high temperatures.
-highs = []
+# Extract date and high temperature.
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
-
 
 # Plot the high temperatures.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
 
 # Format plot.
-ax.set_title("Daily High Temperature, July 2021", fontsize=24 )
+ax.set_title("Daily High Temperature,2021", fontsize=24)
 ax.set_xlabel(" ", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature(F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
