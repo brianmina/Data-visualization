a94a73f76082d45ff3fc4b37c7304c37f726cae2
brianmina <brian.mina17@gmail.com>
2023-12-27 14:40:56 -0500

    working with API using "requests"

--- eq_world_map.py
+++ eq_world_map.py
@@ -15,15 +15,22 @@ path.write_text(readable_content)
 # Examine all earthquakes in the data sheet
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(eq_title)
 
 title = "Global Earthquakes"
-fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+                     color=mags,
+                     color_continuous_scale='Viridis',
+                     labels={'color': 'magnitude'},
+                     projection='natural earth',
+                     hover_name=eq_titles)
 fig.show()
